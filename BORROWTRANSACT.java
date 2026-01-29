a81fd607d9047d6960a469e36f0f4ec9114113a9
reytdn <reynaldotuden88@gmail.com>
2026-01-29 14:36:05 +0800

    task

---
+++ BORROWTRANSACT.java
@@ -0,0 +1,19 @@
+public class BORROWTRANSACT {
+
+    //Processing Details
+    public void borrowBook(BOOKS books){
+        System.out.println("BOOK BORROWED");
+        System.out.println();
+        System.out.println("Book Type: " + books.gettype());
+        System.out.println("Book Genre: " + books.getgenre());
+        System.out.println("Book Title: " + books.gettitle());
+        System.out.println("Book Borrowed Time: " + books.getborrowtime());
+        System.out.println("hi");
+<<<<<<< HEAD
+        System.out.println("RIO");
+        
+=======
+        System.out.println("yey");
+>>>>>>> master
+    } 
+}
